69904be6a68b9e242c104437ef5d9275786dfd18
Ahmad Rais <Ahmad.rdj2@gmail.com>
2020-10-22 14:12:17 +0700

    Add project files.

---
+++ TheCashier/Item.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCashier
+{
+    class Item
+    {
+        private int id;
+        public string tittle { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string tittle, int quantity, double price, double subtotal, string type)
+        {
+            this.id = id;
+            this.tittle = tittle;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = subtotal;
+            this.type = type;
+        }
+
+        public string getTitle()
+        {
+            return tittle;
+        }
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public double getSubTotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+    }
+}
---
+++ TheCashier/calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCashier
+{
+    class calculator
+    {
+        private List<Item> listItem;
+        private double total = 0;
+
+        public calculator()
+        {
+            this.listItem = new List<Item>();
+
+        }
+        public void addItem(Item item)
+        {
+            this.listItem.Add(item);
+            this.total += item.getSubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+        public List<Item> getListItem()
+        {
+            return listItem;
+        }
+    }
+}
